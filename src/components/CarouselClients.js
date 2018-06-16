e13c53c99a62132f4dd61e420851752093b56c85
krs2000 <krskrysiak@gmail.com>
2018-06-16 20:48:43 +0200

    make it better

---
+++ src/components/CarouselClients.js
@@ -0,0 +1,16 @@
+import Carousel from 'nuka-carousel';
+import React from 'react';
+export default class extends React.Component {
+    render() {
+      return (
+        <Carousel>
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+        </Carousel>
+      );
+    }
+  }
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import a from "../img/Clients/1.png";
 import b from "../img/Clients/2.png";
 import c from "../img/Clients/3.png";
 import d from "../img/Clients/4.png";
 import e from "../img/Clients/5.png";
+import Carousel from './CarouselClients'
 
-const ClientsStyle = {
-  color: "white",
-  height: "100vh",
-  display: "flex",
-  "align-items": "center",
-  "justify-content": "center",
-  "flex-direction": "column"
-};
 
-export default function Clients() {
+
+
+
+// const events = {
+//   onDragged: function(event) {...},
+//   onChanged: function(event) {...}
+// };
+
+
+export default class Clients extends Component{
+  render() {
+
+
   return (
     <div className="clients">
     <div className="clientsContainer">
    
-        <h2 className="d-md-none pt-5 m-auto text-center">Clients.</h2>
+        <h2 className="d-md-none pt-5 pb-5 m-auto text-center">Clients</h2>
 
-      <div className="images d-flex flex-column flex-md-row  align-items-center justify-content-center">
-         <a href="https://amber-hotel.pl/en/" target="_blank"><img className="clientImg" src={a} alt="" /></a>
-         <a href="http://r4y.pl/" target="_blank"><img className="clientImg" src={b} alt="" /></a>
-        <a href="http://boatandbike.pl/" target="_blank"><img className="clientImg" src={c} alt="" /></a>
-         <a href="http://www.miastoaniolow.com.pl/" target="_blank"><img className="clientImg" src={d} alt="" /></a>
-         <a href="http://sierakowice.pl/" target="_blank"><img className="clientImg" src={e} alt="" /></a>
+      <div className="images d-none d-md-flex flex-column flex-md-row  align-items-center justify-content-center">
+         <a href="https://amber-hotel.pl/en/" target="_blank" className="d-flex flex-column align-items-center justify-content-center"><h3>Hotel Amber</h3><img className="clientImg" src={a} alt="" /></a>
+         <a href="http://r4y.pl/" target="_blank" className="d-flex flex-column align-items-center justify-content-center"><h3>R4U</h3><img className="clientImg" src={b} alt="" /></a>
+        <a href="http://boatandbike.pl/" target="_blank" className="d-flex flex-column align-items-center justify-content-center"><h3>Boat & Bike</h3><img className="clientImg" src={c} alt="" /></a>
+         <a href="http://www.miastoaniolow.com.pl/" target="_blank" className="d-flex flex-column align-items-center justify-content-center"><h3>Miasto Aniołów</h3><img className="clientImg" src={d} alt="" /></a>
+         <a href="http://sierakowice.pl/" target="_blank" className="d-flex flex-column align-items-center justify-content-center"><h3>Sierakowice Region</h3><img className="clientImg" src={e} alt="" /></a>
       </div>
+  
+      <Carousel/>>
     </div>
     </div>
   );
+}
 }
