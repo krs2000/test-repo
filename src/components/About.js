17740e0cfa8ec1874aa21d8688c468537a3d861a
krs2000 <krskrysiak@gmail.com>
2018-06-23 23:09:55 +0200

    about

--- src/components/About.js
+++ src/components/About.js
@@ -2,45 +2,64 @@ import React from "react";
 import camera from "../img/about/camera.jpg";
 import dron from "../img/about/dron.jpg";
 import forest from "../img/about/forest.jpg";
+import { Footer } from "./footer";
+export default class About extends React.Component {
+  
 
-export default function About() {
-  return (
-    <div className="aboutContainer">
-      <h2 className="d-md-none"> About</h2>
-      <div className="aboutContent">
-
-        <img src={camera} alt="camera oprerator" />
-        <img src={dron} alt="dron oprerator" />
-        <img src={forest} alt="camera forest" />
+  render() {
 
 
-        <h3>
-          <strong> Offbeat motion </strong>is a video production studio founded by Rafał
-           Lipiński.
+
+    return (
+      <div className="aboutContainer">
+        <h2 className="d-md-none"> About</h2>
+        <div className="aboutContent">
+          <div className="aboutMainImg "></div>
+          {/* <img src={camera} alt="camera oprerator"/> */}
+          {/* <img src={dron} alt="dron oprerator" />
+        <img src={forest} alt="camera forest" /> */}
+
+
+
+          <div className='row m-0'>
+            <div className="col-md-6">
+              <div className="row m-0">
+                <div className="col-md-12  justify-content-center">
+                  <h3 className="col-12 col-md-6 ml-auto mt-5 mb-5  justify-content-start">
+                    <strong> Offbeat motion </strong>is a video production studio founded by Rafał
+                     Lipiński.
           </h3>
-
-        <article>
-          I am filmmaker based in the Netherlands, work worldwide. I
-          specialize in commercials, music videos, promotional films,
-          corporate films and event coverage. I take care of it all - starting
-          from script through arranging a set, ligthing, shooting, editing and
-          color grading. For bigger projects I organize and cooperate with
-          film crew. I don't treat making films as a work, cinematography is
-          my passion, most of my time a spend on constant developing skills. I
-          always follow modern standards.
+                  <article className="col-12 col-md-6 ml-auto mb-3 mt-5">
+                    I am filmmaker based in the Netherlands, work worldwide. I
+                    specialize in commercials, music videos, promotional films,
+                    corporate films and event coverage. I take care of it all - starting
+                    from script through arranging a set, ligthing, shooting, editing and
+                    color grading. For bigger projects I organize and cooperate with
+                    film crew. I don't treat making films as a work, cinematography is
+                    my passion, most of my time a spend on constant developing skills. I
+                    always follow modern standards.
           </article>
-
-        <article>
-          <strong>
-            {" "}
-            <br />
-            Every new project I treat as a challange. My main principle is the
-            originality of each video. Better go and watch my work, you will
-            see that every project is different and uniqe.
+                  <article className="col-12 col-md-6 ml-auto mt-3 mb-5">
+                    <strong>
+                      {" "}
+                      <br />
+                      Every new project I treat as a challange. My main principle is the
+                      originality of each video. Better go and watch my work, you will
+                      see that every project is different and uniqe.
             </strong>
-        </article>
-      </div>
-    </div>
+                  </article>
+                </div>
+              </div>
 
-  );
+            </div>
+            <div className="col-12 mb-0 ml-0 mt-0 p-0 col-md-6 dronImgContainer d-flex justify-content-center align-items-center mr-auto">
+              <div className='dronImg mr-auto'></div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+
+    );
+  }
 }
